d5ed3a3e1802a04850030d96fd1e6225cbb6806f
Jack Sutton <jack.sutton@madetech.com>
2022-05-06 15:13:01 +0100

    fixed cors

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,12 @@ module Witchapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'localhost:3000', /https*:\/\/.*?bloopist\.com/
+        resource '*', :headers => :any, :methods => :any
+      end
+    end
 
     # Configuration for the application, engines, and railties goes here.
     #
