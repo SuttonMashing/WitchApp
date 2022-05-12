3109b14aeaa1aac971e2cc943e76d1308376277d
Jack Sutton <jack.sutton@madetech.com>
2022-05-12 15:05:02 +0100

    trying to fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -28,12 +28,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_05_152248) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "initiative", force: :cascade do |t|
-    t.string "name"
-    t.integer "initiative"
-    t.integer "hp"
-    t.string "condition"
-  end
-
 end
