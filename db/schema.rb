a322a06215bd2d76e25b2083f4996845a12e044c
Jack Sutton <jack.sutton@madetech.com>
2022-05-12 14:46:57 +0100

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -29,11 +29,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_05_152248) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "initiative", force: :cascade do |t|
+    t.string "name"
+    t.integer "initiative"
+    t.integer "hp"
+    t.string "condition"
   end
 
 end
