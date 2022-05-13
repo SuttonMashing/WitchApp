92c9a8d2cbfd1c9184632b04a47797ffd3f46227
Jack Sutton <jack.sutton@madetech.com>
2022-05-13 11:20:34 +0100

    added migration to make initiative table

---
+++ db/migrate/20220513101708_add_initiative_table.rb
@@ -0,0 +1,12 @@
+class AddInitiativeTable < ActiveRecord::Migration[7.0]
+    def change
+      create_table :initiative do |t|
+        t.string :name
+        t.string :condition
+        t.integer :initiative
+        t.integer :health
+  
+        t.timestamps
+      end
+    end
+end
