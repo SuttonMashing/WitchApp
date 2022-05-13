9a82ff090f3ed1e0d721872b448a5eac00cdc3ba
Jack Sutton <jack.sutton@madetech.com>
2022-05-13 11:49:29 +0100

    still fixing

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :hitherencounters
       resources :downfallcitizens
-      resources :initiative
+      resources :initiatives
     end
   end
 end
