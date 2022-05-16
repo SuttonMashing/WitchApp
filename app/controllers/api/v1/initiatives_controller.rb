a66249bc6d1884cb777fd9cedef633ac2aefccfa
Jack Sutton <jack.sutton@madetech.com>
2022-05-16 13:57:22 +0100

    update controllers

--- app/controllers/api/v1/initiatives_controller.rb
+++ app/controllers/api/v1/initiatives_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::InitiativesController < ApplicationController
         end
     end
 
-    def put 
+    def patch 
         @initiatives = Initiative.find(params[:id])
         if @initiatives
             @initiatives.update(user_params)
