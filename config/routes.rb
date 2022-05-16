b40985f1f770a8dd72ee60dac43c90d2932f58b3
Jack Sutton <jack.sutton@madetech.com>
2022-05-16 11:13:30 +0100

    updated intitiative controller to match front end

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :hitherencounters
-      resources :downfallcitizens
-      resources :initiatives
+      map.resources :hitherencounters
+      map.resources :downfallcitizens
+      map.resources :initiatives
     end
   end
 end
