2884c6e654b1830580ead7e61b062fb91f670afc
Jack Sutton <jack.sutton@madetech.com>
2022-05-16 11:20:59 +0100

    update controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      map.resources :hitherencounters
-      map.resources :downfallcitizens
-      map.resources :initiatives
+      resources :hitherencounters
+      resources :downfallcitizens
+      resources :initiatives
     end
   end
 end
